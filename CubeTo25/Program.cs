55050a05bb05ec7a6b85c88d63c953bfa5471174
Alexander Kidd <alexander.t.kidd@gmail.com>
2021-09-27 10:16:45 -0400

    Add project files.

---
+++ CubeTo25/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CubeTo25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var xn = 1;
+            while (xn <= 25)
+            {
+                var cube = xn * xn * xn;
+                Console.WriteLine($"{cube}");
+                xn += 2;
+            }
+        }
+    }
+}
